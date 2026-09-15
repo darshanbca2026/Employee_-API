d587139bb05354e4430cafabdc41bc97408703f9
Darshan <dd918673@gamil.com>
2026-09-15 11:10:38 +0530

    fixed db url

--- main.py
+++ main.py
@@ -19,9 +19,6 @@ def get_db():
         yield db
     finally:
         db.close()
-@app.get("/")
-def root():
-    return {"message": "Employee API is Live da! Go to /docs"}
 @app.post("/register")
 def register(
     user: schemas.UserCreate,
